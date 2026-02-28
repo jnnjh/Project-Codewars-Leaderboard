d877de4690cbcb2bb9e6de79b1af49616a225f07
jnnjh <joannejhoieacar@gmail.com>
2026-02-28 21:40:06 +0100

    added function to get users' languages of katas solved/ranked

--- Project-Codewars-Leaderboard/src/leaderboard.mjs
+++ Project-Codewars-Leaderboard/src/leaderboard.mjs
@@ -1,1 +1,17 @@
 //will be used for data handling like sorting and filtering
+
+const languages = [];
+
+export function getLanguages(users) {
+    for(let user of users) {
+        let availableLanguages = user.ranks.languages
+
+        for(let [language, val] of Object.entries(availableLanguages)) {
+            if(!languages.includes(language)) {
+                languages.push(language);
+            }
+        }
+    }
+    languages.sort();
+    return languages;
+}
