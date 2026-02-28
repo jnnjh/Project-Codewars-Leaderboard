4268f6b4308dbe9b41fe1711cbf274aae99dd563
jnnjh <joannejhoieacar@gmail.com>
2026-02-28 19:10:02 +0100

    added parsing function logic to split username inputs that are separated by comma

--- Project-Codewars-Leaderboard/src/utility.mjs
+++ Project-Codewars-Leaderboard/src/utility.mjs
@@ -1,1 +1,6 @@
 //will be used for small helper logics like parsing names etc.
+
+export function parseUsernames(usernames) {
+    const usernameInput = usernames.split(",").map(name => name.trim());
+    console.log(usernameInput);
+}
