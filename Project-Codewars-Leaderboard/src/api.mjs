3a23398c0768df9afe1535d014a97f76b2e34183
jnnjh <joannejhoieacar@gmail.com>
2026-03-02 18:03:44 +0100

    added a function to fetch user

--- Project-Codewars-Leaderboard/src/api.mjs
+++ Project-Codewars-Leaderboard/src/api.mjs
@@ -1,1 +1,20 @@
 // will be used for all fetching logics and error handlings
+
+async function fetchUser(username) {
+    const url = "https://codewars.com/api/v1/users/" + username;
+
+    try {
+        const response = await fetch(url);
+
+        if(!response.ok) {
+            throw new Error (`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+fetchUser("jnnjh");
