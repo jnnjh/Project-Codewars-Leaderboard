99e0ac76b373420b163730c60d62d79b528c87b2
jnnjh <joannejhoieacar@gmail.com>
2026-03-10 20:30:34 +0100

    connecting UI file to project modules

--- Project-Codewars-Leaderboard/src/index.mjs
+++ Project-Codewars-Leaderboard/src/index.mjs
@@ -1,8 +1,3 @@
-// This is a placeholder file to show how you can "mock" fetch requests using
-// the nock library.
-// You can delete the contents of the file once you have understood how it
-// works.
-
-export function makeFetchRequest() {
-  return fetch("https://example.com/test");
-}
+import { parseUsernames } from "./utility.mjs";
+import { fetchUser, fetchAllUsers } from "./api.mjs";
+import { getLeaderboardData } from "./leaderboard.mjs";
