cb3e3eb20d85fe9c4e473f4879bc663bb0e93ab3
jnnjh <joannejhoieacar@gmail.com>
2026-03-10 20:34:20 +0100

    handle form submission and parse usernames

--- Project-Codewars-Leaderboard/src/index.mjs
+++ Project-Codewars-Leaderboard/src/index.mjs
@@ -1,3 +1,15 @@
 import { parseUsernames } from "./utility.mjs";
 import { fetchUser, fetchAllUsers } from "./api.mjs";
 import { getLeaderboardData } from "./leaderboard.mjs";
+
+const form = document.querySelector("#user-form");
+const input = document.querySelector("#user-input");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const inputValue = input.value;
+  const usernames = parseUsernames(inputValue);
+
+  console.log(usernames);
+});
